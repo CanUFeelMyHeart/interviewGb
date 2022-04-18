a76b6ac3541c46e7c4b2d2938966ee307179004d
Mikhail <msmerkushov123@yandex.ru>
2022-04-18 21:41:03 +0300

    copy-function is set

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const copyArray = (testArray) => {
-    resultArray = Array.from(testArray)
+// Создание стрелочной функции с указанием входного массива
+
+arrayForTest = ['test1','test2']
+
+const copyArray = (arrayForTest) => {
+    resultArray = Array.from(arrayForTest)
     return resultArray
 }
 
-console.log(copyArray([1,2,3]))
+console.log(copyArray(arrayForTest))
+
+const equalArrays = (firstArray,secondArray) => {
+
+    if (firstArray.length != secondArray.length) return false; // Мас­си­вы раз­ной дли­ны не рав­ны
+  
+    for(var i = 0; i < firstArray.length; i++) // Цикл по всем эле­мен­там
+        if (firstArray[i] !== secondArray[i]) return false; // Ес­ли хоть один эле­мент от­ли­ча­ет­ся, мас­си­вы не рав­ны
+  
+    return true; // Ина­че они рав­ны
+}
+
+// Тест №1: Сравнение ссылок и значений массива
+const testCopyArray = (resultArray,testArray) => {
+    if (equalArrays(resultArray,testArray) && resultArray != testArray) return true
+    // Если указатели не равны, то новый массив не являются ссылкой на предыдущий
+    else return false
+}
+
+
+console.log(testCopyArray(copyArray(arrayForTest),arrayForTest))
